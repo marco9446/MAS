0c127397dec10b3ec1188c444d143a37321a972b
marco Ravazzini <marco9446@gmail.com>
2014-11-21 14:26:11 +0100

    non working router

---
+++ polymet_test/public/javascripts/functions.js
@@ -0,0 +1,8 @@
+/**
+ * Created by marcoravazzini on 21/11/14.
+ */
+function cinghiale() {
+    console.log("dagsdgghvj");
+    var cd = document.getElementById("sdv").value;
+    document.getElementById("hh").value = cd;
+}
