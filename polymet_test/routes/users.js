1f39a63cf3e4c9a8bf8b3b5df0cced1a18b16e4b
marco Ravazzini <marco9446@gmail.com>
2014-11-21 11:32:12 +0100

    other tests

--- polymet_test/routes/users.js
+++ polymet_test/routes/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+  res.json({
+    ciao:"asd",
+    cane:"http://lightstorage.ilcittadinomb.it/mediaon/cms.newilcittadinomb/storage/site_media/media/photologue/2014/4/25/photos/cache/monza-ciao-a-tutti-dallenpa-ecco-chi-cerca-una-casa_cbf9f12a-a5d9-11e3-90ff-e5a46b97fa06_big_story_linked_ima.jpg"});
 });
 
 module.exports = router;
