1892ce53b98777cd4794aaa61a5816362db6d671
kubamahnert <mahnert@outlook.com>
2014-11-27 16:09:47 +0100

    index fix

--- polymet_test/models/index.js
+++ polymet_test/models/index.js
@@ -2,7 +2,6 @@
 
 var mongoose = require('mongoose');
 
-require('./Action');
 require('./Device');
 require('./Log');
 require('./Module');
@@ -10,7 +9,6 @@ require('./User');
 require('./Program');
 
 module.exports = {
-  'Action' : mongoose.model('Action'),
   'Device' : mongoose.model('Device'),
   'Log' : mongoose.model('Log'),
   'Module' : mongoose.model('Module'),
