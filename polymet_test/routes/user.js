00716fae32d742405dbb142e03f1491ac0741fef
kubamahnert <mahnert@outlook.com>
2014-11-27 16:33:48 +0100

    filter password field!

--- polymet_test/routes/user.js
+++ polymet_test/routes/user.js
@@ -8,13 +8,15 @@ var mongoose = require('mongoose');
 var ObjectId = mongoose.Types.ObjectId;
 var User = mongoose.model('User');
 
+
+passfilter = {"password": 0};
 //allowed methods
 router.all('/', middleware.supportedMethods('GET, POST'));
 
 //get user with id
 router.get('/byid/:userid', function(req, res, next) {
 
-  User.findById(req.params.userid).lean().exec(function(err, user){
+  User.findById(req.params.userid, passfilter).lean().exec(function(err, user){
     if (err) return next (err);
 
     if (!user) {
@@ -28,7 +30,7 @@ router.get('/byid/:userid', function(req, res, next) {
 
 //get user by name
 router.get('/byname/:username', function(req, res, next) {
-    User.find({name: req.params.username}).lean().exec(function(err, user) {
+    User.find({name: req.params.username}, passfilter).lean().exec(function(err, user) {
         if (err) return next (err);
 
         if (!user) {
@@ -50,7 +52,7 @@ router.post('/', function(req, res, next) {
 //get all users
 
 router.get('/', function(req, res, next) {
-    User.find({}).lean().exec(function(err, users) {
+    User.find({}, passfilter).lean().exec(function(err, users) {
         if (err) return next (err);
 
         res.json(users);
