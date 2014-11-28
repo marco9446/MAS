694662c3fbcb73ead0fc8cde0f11b07fb3492852
bedux <bedulm@usi.ch>
2014-11-28 12:00:43 +0100

    Database arduino up
    update arduino communication with database,
    add query class for access to the database

--- polymet_test/routes/arduinoComunication.js
+++ polymet_test/routes/arduinoComunication.js
@@ -1,32 +1,119 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var io=function(){};
-io.router=express.Router();
-
 var http=require('http');
 var ip = require("ip");
-
+var query = require("./query");
+var io=function(){};
+io.router=express.Router();
 io.http=require('http');
 
 
-mongoose.connect("mongodb://localhost/" + "arduinoBaseConnection");
-require("../model");
-ArduinoBase = mongoose.model("ArduinoBase");
+Module = mongoose.model("Module");
+Device = mongoose.model("Device");
 
+var Queue=function(){ 
+    var list=[];
+    var timer=null;
+    var callBack=function(a){
+      console.log("maCane",a.ID,a.PARAM);
+      var ID=a.ID;
+      var message=a.PARAM;
+      try{
+          console.log("send message to ",ID,message);
+          Module.findOne({_id:ID},function(err,found){
+          if(found){
+
+              var finalMsg="";
+              for (i in message){
+                  finalMsg+=Object.keys(message[i])[0].replace("in","")+"=";
+                  finalMsg+=message[i][Object.keys(message[i])[0]]==true?"t&":"f&";
+                  found.PORT[Object.keys(message[i])[0]].state=message[i][Object.keys(message[i])[0]];
+                  found.markModified('PORT');
+                  found.save();
+             }
+              found.save();
+              var options = {
+                    host: found.IP,
+                    port: 80,
+                    path:finalMsg,
+                    method: 'GET'
+                  };
+
+              http.get(options);
+            }
+
+      })
+      }catch(err){
+
+        console.log(err);
+     }}
+
+
+     this.performQuery = function(elem){
+        var helper={};
+        for(var i = 0 ; i < list.length ; i++){
+            if(list[i].ID == elem.ID){
+              for( var l = 0;l<elem.PARAM.length;l++){
+                  list[i].PARAM.push(elem.PARAM[l]);
+              }
+              return;
+            }
+        }
+        list.push(elem);
+     }
+
+    //element = {ID:id,MESSAGE:msg}
+    this.AddElement=function(elem){
+      //OPTIMIZE THE ELEMENT ADDED
+      console.log("add element")
+      this.performQuery(elem);
+      this.start();
+    }
+
+    var execute=function(){
+      if(list.length >0){
+            callBack(list[0]);
+            list.splice(0,1);
+            console.log("do elem in pos ",list.length,0);
+             timer=setTimeout(function() {
+              execute();
+            },2000);
+            console.log("run timeout");   
+      }else{
+         timer=null;
+         console.log("no more element");
+      }
+
+    }
+    this.start=function(){
+        //TODO
+        //Set timer that call execute function every time
+        if(timer==null){
+          console.log("no stuff restart all the timer")
+          execute();
+        }else{
+
+          console.log("in coda")
+        }
+
+    }}
+
+
+
+io.addNewModuleCallback=[];
 io.addNewDeviceCallback=[];
-io.addDeviceCallback=[];
+io.logModuleCallback=[];
 io.actionLinstener=[];
-
-
+io.queue=new Queue();
 
 io.router.post('/', function(req, res) {
-  console.log("send REQ");
   var b=req.body;
   var elem=Object.keys(b)[0];
   var ek1=JSON.parse(elem);
-  io.sendMessageTo(ek1.ID,[ek1.PARAM]);
+  io.queue.AddElement(ek1);
   res.status(200).end();
 });
+
 
 
 io.server1=http.createServer(function (req, res) {
@@ -34,57 +121,56 @@ io.server1=http.createServer(function (req, res) {
   req.on('data', function (chunk) {
     body += chunk;
   });
-  	req.on('end', function () {
-  	console.log(body);
-  
-  	//check if arduino module arleady exists
-  	var content=body.split("\n");
-  	ArduinoBase.findOne({MAC:content[2].replace(/(\r\n|\n|\r)/gm,"")},function(err,found){
-
-  		if(!err){
-  				if(found){
-  					   console.log("trovatoo");
-  						if(found.IP!=content[1].replace(/(\r\n|\n|\r)/gm,"")){
-  							found.IP=content[1].replace(/(\r\n|\n|\r)/gm,"");
-  							found.update(function(err){
-  								for (i in io.addDeviceCallback){
-  									io.addDeviceCallback[i](found);
-  								}
-  							});  							
-  						}else{
-								for (i in io.addDeviceCallback){
-  									io.addDeviceCallback[i](found);
-  								}
-  						}
-  				}else{
-  						v1=ArduinoBase();
-  						v1.MAC=content[2].replace(/(\r\n|\n|\r)/gm,"");
-  						v1.IP=content[1].replace(/(\r\n|\n|\r)/gm,"");
-  						var pinConfig = content[4].split(",");
-              var resPinConfig={};
-              for(i in pinConfig){
-                  resPinConfig['pin'+pinConfig[i][0]]=JSON.parse('{"type":"'+pinConfig[i][2]+'","state":"false"}');
+    req.on('end', function () {
+      console.log(body);
+      var content=body.split("\n");
+      content[2]=content[2].replace(/(\r\n|\n|\r)/gm,"");
+      query.getByNetId(content[2],function(err,found){
+          if(found){
+              var ip=content[1].replace(/(\r\n|\n|\r)/gm,""); 
+              if(found.ip!=ip){
+                //change IP
+                query.changeIp({ip:ip},found,function(){});
+              
               }
-              v1.TYPE=content[3].replace(/(\r\n|\n|\r)/gm,"");
-              v1.PORT=resPinConfig;
-  						v1.save(function(err){
-                  console.log(v1);
-  								for (i in io.addNewDeviceCallback){
-  									io.addNewDeviceCallback[i](v1);
-  								}
-  							
-  						});
-  				}
-  		}
-  	});
-  
-   	res.end();
-  	}
-  );
+              //call calback
+              console.log("mamma mia dammi cento lire che in america voglio andar")
+              for(i in io.logModuleCallback.length){
+
+                io.logModuleCallback[i](found);
+              }
+
+          }else{
+
+              var ip=content[1].replace(/(\r\n|\n|\r)/gm,""); 
+              var netID=content[2].replace(/(\r\n|\n|\r)/gm,""); 
+              var type=content[3].replace(/(\r\n|\n|\r)/gm,"");
+
+              query.addNewModule({ip:ip,type:type,netID:netID},function callBack(err,saved){ 
+                  console.log(err,saved);
+                  var arr= content[4].split(",");
+                  for(var i in arr){
+                      var pin="pin"+arr[i].substring(0,arr[i].indexOf('='));
+                      var type=arr[i].substring(arr[i].indexOf('=')+1);
+
+                      query.addNewDevice({pin:pin,type:type,state:false},function(err1,dSaved){
+                      console.log(err1,dSaved);
+                      for (var i in io.addNewDeviceCallback){
+                            io.addNewDeviceCallback[i](dSaved);
+                          }
+                      console.log(saved._id)
+                      query.pushDevices({id:dSaved._id},saved._id,function(err,upp){
+                          for (var i in io.addNewModuleCallback){
+                            io.addNewModuleCallback[i](upp);
+                          }
+                        });
+                      });
+                  }
+                });
+           }
+        });
+  });
 }).listen(3005,ip.address());
-
-
-
 
 
 io.server2=http.createServer(function (req, res) {
@@ -93,30 +179,38 @@ io.server2=http.createServer(function (req, res) {
     body += chunk;
   });
     req.on('end', function () {
+      console.log("asd");
       var ip=req.connection.remoteAddress;
-      ArduinoBase.findOne({IP:ip},function(err,found){
-        if(found){
-          var elems=body.split(",");
-          var change={};
-          for(elem in elems){
-              elems[elem]=elems[elem].replace("t",'"true"');
-              elems[elem]=elems[elem].replace("f",'"false"');
-              elems[elem]=elems[elem].replace("p",'"pin');
-              elems[elem]=elems[elem].replace(":",'":');
-              var jsonElm=JSON.parse(elems[elem]);
-              var elem=Object.keys(jsonElm)[0];
-              if(found.PORT[elem].state==jsonElm[elem]){}
-              else{
-                change[elem]=jsonElm[elem];
-                found.PORT[elem].state=jsonElm[elem];
-                found.markModified('PORT');
-                found.save();
-              }
-          }
-          console.log(change);
-          for (i in io.actionLinstener){
-              io.actionLinstener[i](change);
-          }           
+      //ArduinoBase.findOne({IP:ip},function(err,found){
+      query.getOneModuleByIp(ip,function(err,found){
+      if(found){
+        var elems=body.split(",");
+        var change=[];
+        //p2=t,p3=f,p4=t .....
+        for(elem in elems){
+            //make the incoming information more readble
+            elems[elem]=elems[elem].replace("t",'true');
+            elems[elem]=elems[elem].replace("f",'false');
+            elems[elem]=elems[elem].replace("p",'pin');
+            var pin = elems[elem].substring(1,elems[elem].indexOf(":")); 
+            var state = elems[elem].substring(elems[elem].indexOf(":")+1,elems[elem].length-1);
+            var p=function(p,s){
+                var mPin=p;
+                var mState=s;
+                query.getOneDeviceFromArrayPin(found.devices,pin,function(err,found){
+                    if(found){
+                      if(found.state!=mState){
+                          query.updateDevieState(found._id,mState,function(err,res){
+                          console.log(res);
+                          for (i in io.actionLinstener){
+                                    io.actionLinstener[i](res);
+                          } 
+                          });
+                      }
+                    }
+                });};
+            p(pin,state);
+        }          
       }
     });
     res.end();
@@ -124,43 +218,6 @@ io.server2=http.createServer(function (req, res) {
   );
 }).listen(3015,ip.address());
 
-
-
-//the message should be   a json with the state of the pin
-//{pin1:true},{pin2:false}]
-io.sendMessageTo=function(ID,message){
-  try{
-  ArduinoBase.findOne({_id:ID},function(err,found){
-      if(found){
-
-        var finalMsg="";
-        console.log(message,"Qidkjfhskjdhf");
-        //TODOa
-        //UPDATE STATE OF ARDUINOOO !!!!!!!!!! Done <3
-        for (i in message){
-
-              finalMsg+=Object.keys(message[i])[0].replace("in","")+"=";
-              finalMsg+=message[i][Object.keys(message[i])[0]]==true?"t&":"f&";
-              found.PORT[Object.keys(message[i])[0]].state=message[i][Object.keys(message[i])[0]];
-              found.markModified('PORT');
-              found.save();
-              //console.log(finalMsg);
-        }
-        found.save();
-        var options = {
-              host: found.IP,
-              port: 80,
-              path:finalMsg,
-              method: 'GET'
-            };
-
-        http.get(options);
-      }
-
-   })
-  }
-  catch(err){}
-}
 
 
 module.exports = io;
---
+++ polymet_test/routes/query.js
@@ -0,0 +1,117 @@
+var query={};
+
+var express = require('express');
+var router = express.Router();
+var middleware =  require('./middleware');
+var mongoose = require('mongoose');
+var ObjectId = mongoose.Types.ObjectId;
+var Module = mongoose.model('Module');
+var Device = mongoose.model('Device');
+
+
+//######################################## ____ Module Method ____#####################################
+
+
+query.getByNetId= function (netID,callback){
+
+	Module.findOne({netID:netID},function(err,found){callback(err,found)});
+}
+
+
+
+//the json should be{key:keyTosearch,value:valToCompare}
+query.getOneModuleBy= function (json,callback){
+
+	Module.findOne(json,function(err,found){callback(err,found)});
+}
+
+query.getOneModuleByIp= function (ip,callback){
+
+	Module.findOne({ip:ip},function(err,found){callback(err,found)});
+}
+
+
+query.addNewModule = function(json,callback){
+
+	var myNewModule=new Module();
+	myNewModule.type = json.type;
+	myNewModule.netID = json.netID;
+	myNewModule.ip = json.ip;
+	myNewModule.save(function(err, saved){
+		callback(err,saved);
+	});
+
+} 
+
+
+//Push device in the devices field of Module
+query.pushDevices=function(json,moduleID,callback){
+
+	Module.findOne({_id:moduleID},function(err,found){
+	if(found){
+	//search the module and in the callback do the stuff below
+		found.devices.push(json.id);
+		found.markModified("devices");
+		found.save(function(err,upp){console.log("pushed",err,upp,found);callback(err,found)});
+	}else{
+
+		console.log("_id ",moduleID," not found")
+	}
+});
+}
+
+
+//Change the ip of some 
+//TODO modules must be replace wuith Id module
+query.changeIp=function(json,modulesID,callback){
+	//TODO search the module and in the callback do the stuff below
+	Module.findOne({_id:moduleID},function(err,found){
+	if(found){
+		found.ip=json.ip;
+		found.markModified("ip");
+		found.save(function(err,saved){callback(err,saved)});
+	}else{
+		console.log("_id ",moduleID," not found")
+	}
+	});
+}
+
+
+
+//######################################## ____Device Method ____#####################################
+
+query.addNewDevice=function(json,callback){
+
+	var myNewModule=new Device();
+	myNewModule.pin   = json.pin;
+	myNewModule.type  = json.type;
+	myNewModule.state = json.state;
+	myNewModule.save(function(err,saved){
+			callback(err,saved);
+	})
+
+}
+
+//TODO id could be a id nont a json
+query.getDevice=function(json,callback){
+
+	Device.findOne({_id:json.id},function(err,dev){callback(err,dev);})
+
+}
+
+query.getOneDeviceFromArrayPin= function(arr,pin,callback){
+	Device.findOne({_id:{$in:arr},pin:pin},function(err,found){callback(err,found)});
+}
+
+
+query.updateDevieState = function(id,newState,callback){
+	
+	query.getDevice({id:id},function(err,found){
+		found.state=newState;
+		found.markModified("state");
+		found.save(function(err,saved){callback(err,saved)});
+	});
+
+} 
+
+module.exports = query;
