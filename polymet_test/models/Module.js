e3ffde948355aa81831cf60b4dfdf5f6e4665a53
bedux <bedulm@usi.ch>
2014-11-28 11:59:01 +0100

    No mac add default name

--- polymet_test/models/Module.js
+++ polymet_test/models/Module.js
@@ -6,11 +6,10 @@ var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var ModuleSchema = new mongoose.Schema(
 {
-	name : {type: String, required: true},
+	name : {type: String, required: true, default:"foo"},
 	type : {type: String, required: true},
-	netID : {type: Number, required: true},
+	netID : {type: String, required: true},
 	ip : {type: String, required: true},
-	mac : {type: String, required: true},
 	devices : {type: [ObjectId], ref: "Device"}
 }
 
