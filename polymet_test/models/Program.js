f4b5bd15c12fb38d88b82eacacddd23c28135dd1
kubamahnert <mahnert@outlook.com>
2014-12-05 14:03:14 +0100

    added "active" property of a Program

--- polymet_test/models/Program.js
+++ polymet_test/models/Program.js
@@ -8,7 +8,8 @@ var ProgramSchema = new mongoose.Schema(
 {
 	name: {type: String, default: "new program"},
 	code: {type: String, required: true},
-	sensors: {type: [ObjectId], ref: "Device"}
+	sensors: {type: [ObjectId], ref: "Device"},
+	active: {type: Boolean, default: true, required: true}
 }
 );
 
